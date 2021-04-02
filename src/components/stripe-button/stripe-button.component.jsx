6e0628efbe413d7fc9d91739df2eb2111c22530e
TwanWestenbroek <twan.westenbroek@gmail.com>
2021-04-02 14:04:42 +0200

    updating stripe integration, favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IblNcDxsgtlTDz4KyRTLiK8d30Wg7Uuev2rPn7Y5qeYyRsaviKnw2BurQ7TrnFOmB6SbgtRGeQGSWuJtH4kCiXL006miyBU9R";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Sylvester Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
