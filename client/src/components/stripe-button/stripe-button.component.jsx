0f04b3a002b755ebeb0b4ef654e696131e74b37c
TwanWestenbroek <twan.westenbroek@gmail.com>
2021-04-13 15:49:52 +0200

    Backend server setup to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey =
-    "pk_test_51IblNcDxsgtlTDz4KyRTLiK8d30Wg7Uuev2rPn7Y5qeYyRsaviKnw2BurQ7TrnFOmB6SbgtRGeQGSWuJtH4kCiXL006miyBU9R";
+  const publishableKey = "pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
     <StripeCheckout
       label="Pay Now"
-      name="Sylvester Clothing Ltd."
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
